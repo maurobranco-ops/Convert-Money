6bc43e2a4ad7adca8401c2df4e1010202bbfcce7
maurobranco <mauro.branco.natal@gmail.com>
2026-02-18 22:00:35 -0300

    Primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+console.log("Funcionou o link do JS")
+
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues(){
+    console.log("Convertendo valores...")
+}
+convertButton.addEventListener("click", convertValues)
+
